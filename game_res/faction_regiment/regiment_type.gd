b933bac2520d6745d7ecb122620aea4e014c6da0
Sven Kanter <svkanter@gmail.com>
2024-07-22 12:21:35 +0200

    added Regiment_Type, stretch mode

--- game_res/faction_regiment/regiment_type.gd
+++ game_res/faction_regiment/regiment_type.gd
@@ -1,3 +1,10 @@
+extends Resource
 class_name Regiment_Type
-extends Node
+
+@export var regiment_name : String = "default name"
+@export var regiment_texture : Texture2D = null
+@export var setup_facing_dir : Vector2 = Vector2.DOWN
+@export var regiment_color : Color = Color.WHITE
+@export var setup_position : Vector2 = Vector2.ZERO
+@export var regiment_unit_size : Vector3 = Vector3.ZERO
 
