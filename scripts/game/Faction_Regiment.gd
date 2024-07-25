18706bd5a891a47fb31fc3b3e057ced9deabc4c0
Sven Kanter <svkanter@gmail.com>
2024-07-25 12:32:11 +0200

    flaggen shader, refactoring

--- scripts/game/Faction_Regiment.gd
+++ scripts/game/Faction_Regiment.gd
@@ -66,6 +66,7 @@ func runntime_ini():
 		var unit = faction_unit_preload.instantiate()
 		var collum =  floor(n / regiment_unit_size.y)
 		unit.position = relative_position  + Vector2(n * unit_pixel_size - collum * regiment_unit_size.y * unit_pixel_size , collum * unit_pixel_size ) + offset
+		unit.visible = false
 		add_child(unit)
 		faction_units.append(unit)
 	guilog(get_rich_common_prefix() + " mit " + str(faction_units.size()) + " Einheiten bereit.")
